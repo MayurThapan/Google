ddd2be4bbf6b5517c78f98b01f0c83ed02a35da1
MayurThapan <Mayuthapan@gmail.com>
2022-04-04 01:35:27 +0100

    Updated code

--- src/main/java/GoogleChalange/GoogleChalange/Solution8.java
+++ src/main/java/GoogleChalange/GoogleChalange/Solution8.java
@@ -7,29 +7,26 @@ public class Solution8
 {
 	
 	private static String a;
-	private static String XOR = "00000000000000000000000000000000";
+	private static String xor = "00000000000000000000000000000000";
 	
     public static int solution(int start, int length) {
     	
     	a= null;
-    	int i,j;
+    	int i;
+    	int j;
     	int counter=length+1;
     	for (i=length ; i> 0; i-- )
     	{for (j=1;j<length+1;j++){
     		if (counter > j && start <=2000000000) {
     		
-    			a= String.format("%32s", Integer.toBinaryString(start)).replaceAll(" ", "0");
-//    		System.out.println(a);
-//    		System.out.println(Integer.parseInt(a, 2));
+    			a= String.format("%32s", Integer.toBinaryString(start)).replace(" ", "0");
     		XORofStringList();
     		}
     		start++;
     		}
     		counter --;
     	}
-    	System.out.println(XOR);
-    	System.out.println(Integer.parseInt(XOR, 2));
-    	return Integer.parseInt(XOR, 2);
+    	return Integer.parseInt(xor, 2);
     }
     
 	  private static void XORofStringList() {
@@ -39,24 +36,24 @@ public class Solution8
 		String tmp2 = null;
 		for (int i = 0; i < 32; i++) {
 			
-			if ( Character.toString(a.charAt(i)).equals("0") &&  Character.toString(XOR.charAt(i)).equals("0") )
+			if ( Character.toString(a.charAt(i)).equals("0") &&  Character.toString(xor.charAt(i)).equals("0") )
 				tmp1 = "0";
-			else if ( Character.toString(a.charAt(i)).equals("0") &&  Character.toString(XOR.charAt(i)).equals("1") )
+			else if ( Character.toString(a.charAt(i)).equals("0") &&  Character.toString(xor.charAt(i)).equals("1") )
 				tmp1 = "1";
-			else if ( Character.toString(a.charAt(i)).equals("1") &&  Character.toString(XOR.charAt(i)).equals("0") )
+			else if ( Character.toString(a.charAt(i)).equals("1") &&  Character.toString(xor.charAt(i)).equals("0") )
 				tmp1 = "1";
-			else if ( Character.toString(a.charAt(i)).equals("1") &&  Character.toString(XOR.charAt(i)).equals("1") )
+			else if ( Character.toString(a.charAt(i)).equals("1") &&  Character.toString(xor.charAt(i)).equals("1") )
 				tmp1 = "0";	
 			
 			if (tmp2==null) tmp2=tmp1;
 			else tmp2=tmp2+tmp1;
 		  }
-		XOR=tmp2;
+		xor=tmp2;
 	}
 	  
 		@Test 
 		  public void tests() { 
-			  solution(500,19);
+			System.out.println( solution(17,4));
 		  }
     
 }
